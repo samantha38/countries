e654d4e7583d990be54047121f8680f231efd0fc
FIRST <anushasj232@gmail.com>
2025-02-16 17:30:31 +0530

    background,ui fixed

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class BirdController : MonoBehaviour
 {
@@ -12,7 +10,6 @@ public class BirdController : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody2D>(); // Ensure Rigidbody2D is assigned
         myRigidbody.gravityScale = 1; // Enable gravity if it was disabled
-        Debug.Log("Bird controller started");
     }
 
     void Update()
@@ -25,10 +22,8 @@ public class BirdController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log($"Collision detected with: {other.gameObject.name}");
         if (other.CompareTag("Letter"))
         {
-            Debug.Log("Letter collected!");
             GameManager.Instance.CollectLetter(other.gameObject.GetComponent<LetterBehavior>().Letter);
             Destroy(other.gameObject);
         }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,6 +11,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject letterPrefab;
     [SerializeField] private Text scoreText;
     [SerializeField] private Text collectedLettersText;
+    [SerializeField] private Text livesText;
+    [SerializeField] private Text gameOverText; // Assign in Unity
     [SerializeField] private float spawnInterval = 3f;
     [SerializeField] private Vector2 spawnAreaMin = new Vector2(-8f, -4f);
     [SerializeField] private Vector2 spawnAreaMax = new Vector2(8f, 4f);
@@ -18,72 +21,49 @@ public class GameManager : MonoBehaviour
     private string currentCountry;
     private List<char> collectedLetters = new List<char>();
     private int score = 0;
-    private float spawnTimer;
-
     private int lives = 3;
-[SerializeField] private Text livesText; // UI element to display lives
-
+    private float spawnTimer;
+    private float correctLetterProbability = 70f; // Start at 70%
 
     void Awake()
     {
         if (Instance == null)
-        {
             Instance = this;
-        }
         else
-        {
             Destroy(gameObject);
-        }
     }
 
     void Start()
     {
+        gameOverText.gameObject.SetActive(false); // Hide "Game Over" at the start
         StartNewRound();
     }
 
-    private void UpdateUI()
-{
-    scoreText.text = $"Score: {score}";
-    collectedLettersText.text = $"Target: {GetCountryWithBlanks()}";
-    
-    if (livesText != null) 
-    {
-        livesText.text = $"Lives: {lives}";
-        Debug.Log($"Updated UI: Lives = {lives}");
-    }
-    else 
-    {
-        Debug.LogError("livesText is not assigned in the Inspector!");
-    }
-}
-
-
-
     void Update()
-{
-    spawnTimer -= Time.deltaTime;
-    if (spawnTimer <= 0)
     {
-        SpawnLetter();
-        spawnTimer = spawnInterval;
-    }
-
-    // Destroy letters that go off-screen
-    foreach (GameObject letter in GameObject.FindGameObjectsWithTag("Letter"))
-    {
-        if (letter.transform.position.x < Camera.main.ViewportToWorldPoint(new Vector3(-0.2f, 0, 0)).x)
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
         {
-            Destroy(letter);
+            SpawnLetter();
+            spawnTimer = spawnInterval;
+        }
+
+        // Destroy letters that go off-screen
+        foreach (GameObject letter in GameObject.FindGameObjectsWithTag("Letter"))
+        {
+            if (letter.transform.position.x < Camera.main.ViewportToWorldPoint(new Vector3(-0.2f, 0, 0)).x)
+            {
+                Destroy(letter);
+            }
         }
     }
-}
 
     private void StartNewRound()
     {
         currentCountry = countries[Random.Range(0, countries.Count)];
         collectedLetters.Clear();
         UpdateUI();
-        
+
         // Clear existing letters
         foreach (GameObject letter in GameObject.FindGameObjectsWithTag("Letter"))
         {
@@ -92,132 +72,128 @@ public class GameManager : MonoBehaviour
     }
 
     private void SpawnLetter()
-{
-    float y = Random.Range(spawnAreaMin.y, spawnAreaMax.y); // Random vertical position
-    float x = Camera.main.ViewportToWorldPoint(new Vector3(1.2f, 0, 0)).x; // Off-screen right
-
-    Vector3 spawnPos = new Vector3(x, y, 0);
-    
-    GameObject letterObj = Instantiate(letterPrefab, spawnPos, Quaternion.identity);
-
-    char letterToSpawn;
-    if (Random.value < 0.7f)
     {
-        letterToSpawn = currentCountry[Random.Range(0, currentCountry.Length)];
+        float y = Random.Range(spawnAreaMin.y, spawnAreaMax.y);
+        float x = Camera.main.ViewportToWorldPoint(new Vector3(1.2f, 0, 0)).x;
+        Vector3 spawnPos = new Vector3(x, y, 0);
+
+        GameObject letterObj = Instantiate(letterPrefab, spawnPos, Quaternion.identity);
+
+        char letterToSpawn;
+        if (Random.value * 100 < correctLetterProbability)
+        {
+            letterToSpawn = currentCountry[Random.Range(0, currentCountry.Length)];
+        }
+        else
+        {
+            letterToSpawn = (char)Random.Range('A', 'Z' + 1);
+        }
+
+        var letterBehavior = letterObj.GetComponent<LetterBehavior>();
+        if (letterBehavior != null)
+        {
+            letterBehavior.Initialize(letterToSpawn);
+        }
+
+        Rigidbody2D rb = letterObj.AddComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(-2f, 0);
     }
-    else
+
+    private void GameOver()
     {
-        letterToSpawn = (char)Random.Range('A', 'Z' + 1);
+        gameOverText.text = "GAME OVER";
+        gameOverText.gameObject.SetActive(true); // Show "Game Over"
+        Time.timeScale = 0; // Stop the game
     }
-
-    var letterBehavior = letterObj.GetComponent<LetterBehavior>();
-    if (letterBehavior != null)
-    {
-        letterBehavior.Initialize(letterToSpawn);
-    }
-
-    // Move letter left
-    Rigidbody2D rb = letterObj.AddComponent<Rigidbody2D>(); // Add Rigidbody2D if not already present
-    rb.gravityScale = 0; // Prevent falling
-    rb.linearVelocity = new Vector2(-2f, 0); // Move left at a speed of 2 units per second
-}
-
-private void GameOver()
-{
-    Debug.Log("Game Over!"); 
-    Time.timeScale = 0; // Stop the game
-    // You can add UI to show "Game Over" message here
-}
 
     public void CollectLetter(char letter)
-{
-    Debug.Log($"GameManager collecting letter: {letter}");
-    
-    if (currentCountry.Contains(letter)) 
     {
-        collectedLetters.Add(letter); // Correct letter
-        CheckWord();
-    }
-    else 
-    {
-        lives--; // Wrong letter, lose a life
-        Debug.Log($"Wrong letter! Lives left: {lives}");
-        
-        if (lives <= 0)
+        if (currentCountry.Contains(letter))
         {
-            GameOver();
-            return;
+            collectedLetters.Add(letter);
+            CheckWord();
         }
+        else
+        {
+            lives--;
+            if (lives <= 0)
+            {
+                GameOver();
+                return;
+            }
+        }
+        UpdateUI();
     }
-    
-    UpdateUI();
-}
-
-
-    private IEnumerable<string> GetPermutations(string source, int length)
-{
-    if (length == 1) return source.Select(x => x.ToString());
-
-    return GetPermutations(source, length - 1)
-        .SelectMany(x => source.Where(y => !x.Contains(y)),
-            (x, y) => x + y);
-}
-
 
     private void CheckWord()
     {
         string collected = new string(collectedLetters.ToArray());
-        
-        // Check if we have enough letters to potentially form the country name
+
         if (collectedLetters.Count >= currentCountry.Length)
         {
-            // Check all possible combinations of collected letters
             var possibleWords = GetPermutations(collected, currentCountry.Length);
             foreach (string word in possibleWords)
             {
                 if (word == currentCountry)
                 {
-                    score += 100;
-                    UpdateUI();
-                    StartNewRound();
+                    score += 10; // Update score
+                    if (lives < 3)
+                        lives++; // Reward 1 life if not max
+
+                    correctLetterProbability = Mathf.Clamp(correctLetterProbability - 3, 30, 70); // Adjust probability
+
+                    StartCoroutine(TransitionToNextCountry()); // Transition immediately
                     return;
                 }
-            }
-            
-            // If we have too many letters and still haven't found a match, clear and start over
-            if (collectedLetters.Count > currentCountry.Length)
-            {
-                collectedLetters.Clear();
-                UpdateUI();
             }
         }
     }
 
-    private char GetNextCorrectLetter()
-{
-    foreach (char letter in currentCountry)
+    private IEnumerator TransitionToNextCountry()
     {
-        if (!collectedLetters.Contains(letter))
-            return letter; // Return the first missing letter
-    }
-    return '\0'; // Return null character if all letters are collected
-}
-
-
-private string GetCountryWithBlanks()
-{
-    string modifiedCountry = "";
-    
-    for (int i = 0; i < currentCountry.Length; i++)
-    {
-        if (i == 0 || collectedLetters.Contains(currentCountry[i]))
-            modifiedCountry += currentCountry[i] + " "; // Show the first letter & collected ones
-        else
-            modifiedCountry += "_ "; // Hide the rest
+        yield return new WaitForSeconds(0.5f); // Short delay before next round
+        StartNewRound();
     }
 
-    return modifiedCountry;
+    private IEnumerable<string> GetPermutations(string source, int length)
+    {
+        if (length == 1) return source.Select(x => x.ToString());
+
+        return GetPermutations(source, length - 1)
+            .SelectMany(x => source.Where(y => !x.Contains(y)), (x, y) => x + y);
+    }
+
+    private string GetCountryWithBlanks()
+    {
+        string modifiedCountry = "";
+        int revealedLetters = 0;
+
+        for (int i = 0; i < currentCountry.Length; i++)
+        {
+            if (i == 0 || collectedLetters.Contains(currentCountry[i]))
+            {
+                modifiedCountry += currentCountry[i] + " ";
+                revealedLetters++;
+            }
+            else if (revealedLetters < 2) // Ensure at least 2 letters are shown
+            {
+                modifiedCountry += currentCountry[i] + " ";
+                revealedLetters++;
+            }
+            else
+            {
+                modifiedCountry += "_ ";
+            }
+        }
+
+        return modifiedCountry;
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.text = $"Score: {score}";
+        collectedLettersText.text = $"Target: {GetCountryWithBlanks()}";
+        livesText.text = $"Lives: {lives}";
+    }
 }
-
-}  
-
